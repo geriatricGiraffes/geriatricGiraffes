e61370db5ae4c0b934157f77aa923867fca5f706
Michael Dziedzic <michaeldzie@gmail.com>
2016-02-19 14:18:07 -0800

    (feat) posts: integrated with Posts service

--- client/app/posts/posts.js
+++ client/app/posts/posts.js
@@ -1,41 +1,21 @@
 angular.module('hackoverflow.posts', [
   'hackoverflow.services'
 ])
+
 .controller('PostsController', function ($scope, Posts) {
-  $scope.posts= [
-    {
-      author: 'Spiney Norman',
-      title: 'What is up with Angular and injection?',
-      data: '5/2/15'
-    },
-    {
-      author: 'Bobby Bobson',
-      title: 'Never forget the tibbles.',
-      data: '5/2/15'
-    },
-    {
-      author: 'Mr. Fancy Pants',
-      title: 'Star Wars will never end.',
-      data: '5/2/15'
-    },
-    {
-      author: 'Gerry Geriatric',
-      title: 'Bobby Bobson. What a name.',
-      data: '5/2/15'
-    }
-  ];
+
+  $scope.posts = [];
 
   $scope.sort = function(arg) {
   }
 
-  $scope.logout = function logout() {
-    Auth.signout();
+  $scope.getPosts = function getPosts() {
+    $scope.posts = Posts.getPosts();
+    // use below version with .then when db is online
+    // Posts.getPosts().then(function(data) {
+    //   $scope.posts = data;
+    // })
   }
 
-  $scope.getPosts = function getPosts() {
-    Posts.getPosts().then(function(data) {
-      $scope.posts = data;
-    })
-  }
-//  $scope.getPosts();
+  $scope.getPosts();
 });
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -4,6 +4,59 @@ angular.module('hackoverflow.services', [])
 
   function getPosts() {
 
+    // db integration to replace test data
+    return [
+      {
+        author: 'Spiney Norman',
+        title: 'What is up with Angular and injection?',
+        created: '5/2/15'
+      },
+      {
+        author: 'Bobby Bobson',
+        title: 'The pros and cons of two-way createdbinding in AngularJS and Angular2',
+        created: '5/2/15'
+      },
+      {
+        author: 'Mr. Fancy Pants',
+        title: 'This, that, bind, call, apply, scope, closure. WTF?',
+        created: '5/2/15'
+      },
+      {
+        author: 'Gerry Geriatric',
+        title: 'Codybot and the perils of community-assisted code manipulation',
+        created: '5/2/15'
+      },
+      {
+        author: 'Spiney Norman',
+        title: 'My hovercraft is full of eels and that is why I used a Backbone collection',
+        created: '5/2/15'
+      },
+      {
+        author: 'Spiney Norman',
+        title: 'What is up with Angular and injection?',
+        created: '5/2/15'
+      },
+      {
+        author: 'Bobby Bobson',
+        title: 'The pros and cons of two-way createdbinding in AngularJS and Angular2',
+        created: '5/2/15'
+      },
+      {
+        author: 'Mr. Fancy Pants',
+        title: 'This, that, bind, call, apply, scope, closure. WTF?',
+        created: '5/2/15'
+      },
+      {
+        author: 'Gerry Geriatric',
+        title: 'Codybot and the perils of community-assisted code manipulation',
+        created: '5/2/15'
+      },
+      {
+        author: 'Spiney Norman',
+        title: 'My hovercraft is full of eels and that is why I used a Backbone collection',
+        created: '5/2/15'
+      }      
+    ];
   }
 
   return {
