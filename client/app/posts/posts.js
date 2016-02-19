de2c03932d96072fbe92fe9c4115ca8bac752646
Michael Dziedzic <michaeldzie@gmail.com>
2016-02-19 13:24:24 -0800

    (feat) posts: added basic looping functionality

--- client/app/posts/posts.js
+++ client/app/posts/posts.js
@@ -2,7 +2,28 @@ angular.module('hackoverflow.posts', [
   'hackoverflow.services'
 ])
 .controller('PostsController', function ($scope, Posts) {
-  $scope.data= [];
+  $scope.posts= [
+    {
+      author: 'Spiney Norman',
+      title: 'What is up with Angular and injection?',
+      data: '5/2/15'
+    },
+    {
+      author: 'Bobby Bobson',
+      title: 'Never forget the tibbles.',
+      data: '5/2/15'
+    },
+    {
+      author: 'Mr. Fancy Pants',
+      title: 'Star Wars will never end.',
+      data: '5/2/15'
+    },
+    {
+      author: 'Gerry Geriatric',
+      title: 'Bobby Bobson. What a name.',
+      data: '5/2/15'
+    }
+  ];
 
   $scope.sort = function(arg) {
   }
@@ -13,8 +34,8 @@ angular.module('hackoverflow.posts', [
 
   $scope.getPosts = function getPosts() {
     Posts.getPosts().then(function(data) {
-      $scope.data = data;
+      $scope.posts = data;
     })
   }
-  $scope.getPosts();
+//  $scope.getPosts();
 });
