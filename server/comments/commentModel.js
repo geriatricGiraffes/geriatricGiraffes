bb7d7359556a5bc754288a81ffb5a1dc16f06ca5
Christopher Patton <chrislpatton@gmail.com>
2016-02-22 18:58:34 -0500

    (feat) required model in controller

---
+++ server/comments/commentModel.js
@@ -0,0 +1,29 @@
+
+var mongoose = require('mongoose');
+
+var CommentSchema = new mongoose.CommentSchema({ 
+	_id : Number,
+	post: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Post'
+	},
+	author: {
+		type:mongoose.Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	body: String,
+	timeStamp: {
+		type:Date,
+		default: Date.now
+	}
+})
+
+
+
+// {
+//     "commentId": 0,
+//     "postId": 0,
+//     "userId": "Spiney Norman",
+//     "body": "Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters.",
+//     "timeStamp": "5/2/15"
+//   },
