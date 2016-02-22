a6e10b0a0688e8eb628e391d6e80741c79d54ef0
danmolitor <danmolitor91@gmail.com>
2016-02-22 13:29:32 -0800

    (feat) middleware.js - coded out functionality. server.js -  added commented out link for eventual use of middleware.js.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,43 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 
-var port = process.env.PORT || 8000;
+// mongoose.connect('mongodb://localhost/hackoverflow'); //connect to mongo database
+
+
+// configure our server with all the middleware and routing
+
+// UNCOMMENT TO USE WITH MIDDLEWARE.JS
+
+// require('./config/middleware.js')(app, express);
+
+//COMMENT OUT TO USE WITH MIDDLEWARE.JS
 
 app.use(express.static(__dirname + '/../client'));
+
+
+var port = process.env.PORT || 8000;
 
 app.listen(port);
 
 module.exports = app;
+
+
+// Comments from Angular Server file.
+
+
+/* Walkthrough of the server
+
+  Express, mongoose, and our server are initialzed here
+  Next, we then inject our server and express into our config/middleware.js file for setup.
+    We also exported our server for easy testing
+
+  middleware.js requires all express middleware and sets it up
+  our authentication is set up there as well
+  we also create individual routers for are two main features, links and users
+  each feature has its own folder with a model, controller, and route file
+    the respective file is required in middleware.js and injected with its mini router
+    that route file then requires the respective controller and sets up all the routes
+    that controller then requires the respective model and sets up all our endpoints which respond to requests
+
+*/
