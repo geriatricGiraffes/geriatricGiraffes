85ab500ee2655d770ad1d623b06a4fe3187eee70
danmolitor <danmolitor91@gmail.com>
2016-02-24 12:41:32 -0800

    (refactor) Refactored serverside post and user setups

--- server/posts/postModel.js
+++ server/posts/postModel.js
@@ -1,16 +1,30 @@
 var mongoose = require('mongoose');
 
 var PostSchema = new mongoose.Schema({
-  author:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref : 'User'
-  },
-  title: String,
-  body: String,
-  created: {
-    type: Date,
-    Default: Date.now
-  }
+ author:{
+ 	type: mongoose.Schema.Types.ObjectId,
+ 	ref : 'User',
+ },
+ title: String,
+ body: String,
+ forum: String,
+ created: {
+ 	type: Date,
+ 	Default: Date.now
+ },
+ comments: [
+    {
+      author:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+      },
+      body: String,
+      created: {
+        type: Date,
+        Default: Date.now
+      }
+    }
+  ]
 });
 
 module.exports = mongoose.model('Post', PostSchema);
--- server/users/userModel.js
+++ server/users/userModel.js
@@ -4,7 +4,9 @@ var UserSchema = new mongoose.Schema({
   oauthID: Number,
   name: String,
   photoUrl: String,
-  created: Date
+  created: Date,
+  posts : [{ type: Schema.Types.ObjectId, ref: 'Posts'}],
+  comments: [{ type: Schema.Types.ObjectId, ref: 'Comments'}]
 });
 
 module.exports = mongoose.model('User', UserSchema);
