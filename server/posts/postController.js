ac0850ea23ed21a99f2ec80fb76d973e24c5ddd5
danmolitor <danmolitor91@gmail.com>
2016-02-25 15:01:20 -0800

    (fix) fixed a line of code that was hindering static file loading

--- server/posts/postController.js
+++ server/posts/postController.js
@@ -1,13 +1,13 @@
 var Post       = require('./postModel.js');
-var Comment    = require('../comments/commentModel.js')
+var Comment    = require('../comments/commentModel.js');
 module.exports = {
 
-  
+
   // gets all posts
   getPosts : function(req, res, next) {
     Post.find(function(err, posts) {
     if (err) { return next(err); }
-    
+
      res.json(posts);
    });
  },
@@ -25,7 +25,7 @@ module.exports = {
     post.body = req.body.body;
     post.save(function(err, post) {
      if(err) { return next(err); }
-    
+
      res.json(post);
    });
  },
@@ -58,18 +58,18 @@ module.exports = {
      }, function(err) {
       if (err) { return next(err)}
      });
-   })
+   });
    Post.remove({
     _id: req.params.post
    }, function(err, post) {
      if (err) { return next(err); }
-    
+
     // get and return all the posts after you delete one
     Post.find(function(err, posts) {
       if (err) { return next(err); }
-      
+
        res.json(posts);
     });
-   })
+   });
  }
 };
