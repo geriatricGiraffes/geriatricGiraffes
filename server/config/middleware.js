1414904aaf2cd5dba21c313eaaf56242ce6485fd
Christopher Patton <chrislpatton@gmail.com>
2016-02-25 17:15:37 -0500

    (refactor) changed an api route

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -14,7 +14,7 @@ module.exports = function ( app, express ) {
 
   app.use('/api/users', userRouter); // use userRouter for all user requests
   app.use('/api/post', postRouter); // use postRouter for all user post requests
-  app.use('/api/post/comments', commentRouter); // use commentRouter for all use comment requests
+  app.use('/api/post', commentRouter); // use commentRouter for all use comment requests
 
   // authentication middleware used to decode token and made available on the request
 // app.use('someroute/someroute', helpers.decode);
